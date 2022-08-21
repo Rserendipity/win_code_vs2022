db82bd50f7b62e4640433ef7cd379fd17a41caf9
“changjingjing” <2249077862@qq.com>
2022-08-21 15:23:46 +0800

    1

--- C/data_struct/数据结构/5图/无向图/无向图/main.c
+++ C/data_struct/数据结构/5图/无向图/无向图/main.c
@@ -2,11 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_VEX 100
 
+typedef struct AMG_Graph {
+    int vexSize;
+    int edgeSize;
+    char vex[MAX_VEX];
+    int edge[MAX_VEX][MAX_VEX];
+} AMG_Graph;
 
+AMG_Graph* AMGinit()
+{
+    AMG_Graph* graph = (AMG_Graph*)calloc(sizeof(AMG_Graph), 1);
+    printf("Input Vex nums:>");
+    scanf("%d", &graph->vexSize);
+    printf("Input Edge nums:>");
+    scanf("%d", &graph->edgeSize);
+
+    return graph;
+}
 
 int main() 
 {
+    AMG_Graph* graph = AMGinit();
+
+
+
 
     return 0;
 }
