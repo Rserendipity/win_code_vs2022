b9d374331d886bec451dcc8a5a08aa62b3bd9a41
“changjingjing” <2249077862@qq.com>
2022-09-12 18:46:09 +0800

    学习常量成员函数

--- Cplusplus/bilibili/4类/this指针.cpp
+++ Cplusplus/bilibili/4类/this指针.cpp
@@ -10,6 +10,7 @@ public:
         // this 指针指向的当前对象的成员
         // 所以用this指针指向的对应的变量就可以找到对应对象的值
         this->data = 10;
+        this->a = 1;
     }
     void setNum(int num)
     {
@@ -18,6 +19,17 @@ public:
     int getNum()
     {
         return this->data;
+    }
+    // 在函数的右边加上const就可以告诉编译器，这是一个常函数
+    // 注：当把const放到左边的时候表示的是返回值是一个常量
+    // 这个函数不会在函数内部更改成员对象的值
+    // 当变量声明成 mutable 时，可以在常函数内更改
+    void printData() const 
+    {
+        // this->data = 100; // err 函数已经被声明成了常函数，内部不可以修改
+        this->a = 100l; // 可以修改，a是 mutable 变量
+        cout << this->data << endl;
+        cout << this->a << endl;
     }
     Data& getThis()
     {
@@ -30,6 +42,9 @@ private:
     // 所有的对象共用所有的方法
     // 
     int data;
+
+    // a变量可以在常函数中被更改
+    mutable int a;
 };
 /// <summary>
 /// this指针
@@ -56,8 +71,33 @@ void test1()
 
 }
 
+/// <summary>
+/// const修饰成员函数
+/// </summary>
+void test2()
+{
+    Data d1;
+    d1.printData();
+}
+
+/// <summary>
+/// const对象
+/// </summary>
+void test3()
+{
+    // 常对象只能调用const修饰的函数
+    // 普通的成员函数被编译器认为是不安全的
+    // 可能发生对成员的修改，而常对象又不允许被修改
+    // 
+    const Data d1; // 构造时，a的值时1
+    // d1中的a是mutable变量，在print函数中修改了他的值
+    d1.printData(); // 打印输出a的值时100
+}
+
 int main() 
 {
-    test1();
+    // test1();
+    // test2();
+    test3();
     return 0;
 }
