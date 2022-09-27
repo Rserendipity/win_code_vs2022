826293629d6a9b2651d99afd4e4c2d26a4837065
“changjingjing” <2249077862@qq.com>
2022-09-27 15:35:06 +0800

    继承中同名的处理

--- Cplusplus/bilibili/5继承/同名成员.cpp
+++ Cplusplus/bilibili/5继承/同名成员.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Base {
 public:
+    static int data;
     int num;
     Base() :num(0)
     {
@@ -17,11 +18,23 @@ public:
     {
         cout << "Base xigou" << endl;
     }
+    void ShowNum()
+    {
+        cout << "fulei:" << num << endl;
+    }
+    void ShowNum(int x)
+    {
+        cout << "fulei:int :" << x << endl;
+    }
 };
+
+int Base::data = 100;
 
 class Son:public Base {
 public:
     int num;
+    static int data;
+
     Son(int x, int y) :num(x), Base(y)
     {
         cout << "Son youcan" << endl;
@@ -32,7 +45,16 @@ public:
         cout << "Base: " << Base::num << endl;
         cout << "Son : " << num << endl;
     }
+
+
+
+    void ShowNum()
+    {
+        cout << "zilei de shownum:" << num << endl;
+    }
 };
+
+int Son::data = 300;
 
 /// <summary>
 /// 数据同名
@@ -48,12 +70,38 @@ void test1()
 /// </summary>
 void test2()
 {
+    // 继承会继承所有的成员函数（除去构造和析构）和成员变量
+    // 构造和析构 只有父类自己知道怎么做
+    // 
+    // 一旦子类实现了一个父类的同名函数
+    // 编译器会屏蔽掉所有的重载函数
+    // 除非加上父类的作用域才可以调用
+    // 
 
+    Son s1(10, 20);
+    s1.ShowNum(); // fulei
+    s1.show();    // zilei
+    s1.Base::ShowNum();
+}
+
+/// <summary>
+/// 静态成员同名
+/// </summary>
+void test3()
+{
+    // 子类和父类的静态变量同名了
+    // 默认访问子类的
+    // 想要访问父类的要加上作用域
+    // 
+    cout << Base::data << endl;
+    cout << Son::data << endl;
+    cout << Son::Base::data << endl;
 }
 
 int main()
 {
-    test1();
-    test2();
+    // test1();
+    // test2();
+    test3();
     return 0;
 }
