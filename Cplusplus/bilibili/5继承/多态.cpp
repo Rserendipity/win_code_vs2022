a2d9f0863c045a2c3a065eb0b33147b6bda6717e
“changjingjing” <2249077862@qq.com>
2022-10-02 16:50:57 +0800

    多态，虚函数

---
+++ Cplusplus/bilibili/5继承/多态.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void Sleep() // 虚函数
+    // void Sleep() // 普通成员函数
+    {
+        cout << "Animal在睡觉" << endl;
+    }
+};
+
+class Dog :public Animal {
+public:
+
+    void Sleep()
+    {
+        cout << "Dog在睡觉" << endl;
+    }
+};
+
+void test1()
+{
+    // 多态：
+    // 静态多态（静态联编，函数地址在编译期间确定）：函数重载，运算符重载
+    // 动态多态（动态联编，函数地址在运行时确定）  ：继承中的虚函数（重写）
+    // 
+    
+    // 复习部分：
+    //Dog d;
+    //d.Sleep(); // dog
+    //d.Animal::Sleep(); // animal
+
+    // 向上兼容性：
+    // 用Animal指针保存Dog的地址，访问的结果只会是dog继承的Animal部分
+    //Animal* p = new Dog;
+    //p->Sleep(); // animal
+
+    // 虚函数：
+    // 基类的虚函数，在子类中重写后，用基类指针就可以操作子类的成员
+    Animal* p = new Dog;
+    p->Sleep(); // dog
+}
+
+int main() 
+{
+    test1();
+    return 0;
+}
--- Cplusplus/bilibili/5继承/虚继承.cpp
+++ Cplusplus/bilibili/5继承/虚继承.cpp
@@ -1,21 +1,39 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <iostream>
-using namespace std;
-
-class Animal {
-public:
-    int data;
-};
-
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
+//
+//class Animal {
+//public:
+//    int data;
+//};
+//
+/////// <summary>
+/////// 普通菱形继承
+/////// </summary>
+////class Yang:public Animal {
+////public:
+////
+////};
+////
+////class Tuo :public Animal {
+////public:
+////
+////};
+////
+////class YangTuo :public Yang, public Tuo {
+////public:
+////
+////};
+//
 ///// <summary>
-///// 普通菱形继承
+///// 虚继承
 ///// </summary>
-//class Yang:public Animal {
+//class Yang:virtual public Animal {
 //public:
 //
 //};
 //
-//class Tuo :public Animal {
+//class Tuo :virtual public Animal {
 //public:
 //
 //};
@@ -24,56 +42,38 @@ public:
 //public:
 //
 //};
-
-/// <summary>
-/// 虚继承
-/// </summary>
-class Yang:virtual public Animal {
-public:
-
-};
-
-class Tuo :virtual public Animal {
-public:
-
-};
-
-class YangTuo :public Yang, public Tuo {
-public:
-
-};
-
-/// <summary>
-/// 菱形继承
-///         Animal
-///         /    \
-///       Yang   Tuo
-///         \   /
-///        YangTuo
-/// </summary>
-void test1()
-{
-    //YangTuo y;
-    //// y.data = 100; // err 多个继承过来的data
-    //y.Yang::data = 100; // 有二义性的时候加上作用域，即可解决问题
-}
-
-/// <summary>
-/// 虚继承，解决菱形继承的二义性以及重复继承问题
-/// </summary>
-void test2()
-{
-    YangTuo y;
-    // 虚继承体系中只会有一份data
-    // 所有的派生类使用虚基类指针和虚基类表共用同一份数据
-    // 解决了二义性，也解决了内存的占用问题
-    y.data = 100; 
-    cout << y.data << endl;
-}
-
-int main() 
-{
-    // test1();
-    test2();
-    return 0;
-}
+//
+///// <summary>
+///// 菱形继承
+/////         Animal
+/////         /    \
+/////       Yang   Tuo
+/////         \   /
+/////        YangTuo
+///// </summary>
+//void test1()
+//{
+//    //YangTuo y;
+//    //// y.data = 100; // err 多个继承过来的data
+//    //y.Yang::data = 100; // 有二义性的时候加上作用域，即可解决问题
+//}
+//
+///// <summary>
+///// 虚继承，解决菱形继承的二义性以及重复继承问题
+///// </summary>
+//void test2()
+//{
+//    YangTuo y;
+//    // 虚继承体系中只会有一份data
+//    // 所有的派生类使用虚基类指针和虚基类表共用同一份数据
+//    // 解决了二义性，也解决了内存的占用问题
+//    y.data = 100; 
+//    cout << y.data << endl;
+//}
+//
+//int main() 
+//{
+//    // test1();
+//    test2();
+//    return 0;
+//}
