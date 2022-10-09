58ee0fa2beb69e3257470887ae830d20a5e6f13a
“changjingjing” <2249077862@qq.com>
2022-10-09 16:48:47 +0800

    完成背单词2.0

--- C/App/背单词/2.0/Word.h
+++ C/App/背单词/2.0/Word.h
@@ -3,17 +3,30 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <Windows.h>
 using namespace std;
 class Word
 {
+	friend void InputModel(Word* word);
+	friend void ClearRecord(Word* word);
+
 public:
-	const int MAX_WORD_SIZE = 4449;
+	const int MAX_WORD_SIZE = 4449;				// 总共的单词数量
+	const char Options[4] = {'A','B','C','D'};  // 
 	Word();
-	string** GetCh();
 	~Word();
+	string GetWord();		 // 获取英文
+	int GetCorrectOption();  // 获取正确的选项
+	void Save();
+	void GetInfo();
 private:
-	static int current;
+	void GetOptions(); // 获取随机的选项
+	Word(const Word&); // 不允许拷贝构造
+	int current;
 	string* ch[4];
-	string* en;
+	string* en; 
+	int randArr[4];
+	fstream fin;
+
 };
 
