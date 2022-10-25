75e40204733bf1351d89132e928bee4054dc2a4e
“changjingjing” <2249077862@qq.com>
2022-10-25 19:12:27 +0800

    leetcode 1450题

---
+++ C/leetcode/1480. 一维数组的动态和/main.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sz = nums.size();
+        for (int i = 1; i < sz; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
+int main()
+{
+
+    return 0;
+}
