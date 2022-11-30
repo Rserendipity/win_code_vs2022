641eff40c9845df0cc8af5dd547859771afb25eb
“changjingjing” <2249077862@qq.com>
2022-11-30 10:39:58 +0800

    86. 分隔链表

---
+++ C/leetcode/86. 分隔链表/main.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* minHead = nullptr;
+        ListNode* maxHead = nullptr;
+        ListNode* minMove = nullptr;
+        ListNode* maxMove = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            if (cur->val < x) {
+                if (minHead == nullptr) {
+                    minHead = minMove = cur;
+                } else {
+                    minMove->next = cur;
+                    minMove = cur;
+                }
+            } else {
+                if (maxHead == nullptr) {
+                    maxHead = maxMove = cur;
+                } else {
+                    maxMove->next = cur;
+                    maxMove = cur;
+                }
+            }
+            cur = cur->next;
+        }
+        if (minHead) {
+            minMove->next = maxHead;
+            if (maxHead) {
+                maxMove->next = nullptr;
+            }
+        } else {
+            minHead = maxHead;
+        }
+
+        return minHead;
+    }
+};
+int main()
+{
+
+    return 0;
+}
