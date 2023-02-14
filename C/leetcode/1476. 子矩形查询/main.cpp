99d7f70a0c0c5497d4182c708da874dff9f4cdc1
“changjingjing” <2249077862@qq.com>
2023-02-14 09:28:42 +0800

    leetcode 1476. 子矩形查询

---
+++ C/leetcode/1476. 子矩形查询/main.cpp
@@ -0,0 +1,27 @@
+class SubrectangleQueries {
+public:
+    SubrectangleQueries(vector<vector<int>>& rectangle) {
+        vv = rectangle;
+    }
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                vv[i][j] = newValue;
+            }
+        }
+    }
+    
+    int getValue(int row, int col) {
+        return vv[row][col];
+    }
+private:
+    vector<vector<int>> vv;
+};
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
+ * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj->getValue(row,col);
+ */
