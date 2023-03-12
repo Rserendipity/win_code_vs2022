8c096c3eb9aeaa321e3caba0800a09de1d73524d
“changjingjing” <2249077862@qq.com>
2023-03-12 09:45:44 +0800

    leetcode 2500. 删除每行中的最大值

---
+++ C/leetcode/2500. 删除每行中的最大值/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        int ans = 0;
+        for (auto& v : grid) {
+            sort(v.begin(), v.end());
+        }
+        for (int i = 0; i < grid[0].size(); i++) {
+            int max = -1;
+            for (int j = 0; j < grid.size(); j++) {
+                max = std::max(max, grid[j][i]);
+            }
+            ans += max;
+        }
+        return ans;
+    }
+};
