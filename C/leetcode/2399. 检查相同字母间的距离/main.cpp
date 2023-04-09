e6a9b28da6a02103724599641fe76f93320bc132
“changjingjing” <2249077862@qq.com>
2023-04-09 10:56:12 +0800

    leetcode 2399. 检查相同字母间的距离

--- C/leetcode/2399. 检查相同字母间的距离/main.cpp
+++ C/leetcode/2399. 检查相同字母间的距离/main.cpp
@@ -1,26 +1,15 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <bits/stdc++.h>
-#include <unordered_map>
-
-using namespace std;
 class Solution {
 public:
-    bool checkDistances(string s, const vector<int>& distance) {
-        unordered_map<char, int> hash;
+    bool checkDistances(string s, vector<int>& distance) {
+        int hash[26] = {0};
+        memset(hash, -1, 26 * sizeof(int));
         for (int i = 0; i < s.size(); i++) {
-            if (hash.insert(make_pair(s[i], i)).second == false) {
-                if (distance[s[i] - 'a'] != i - hash[s[i]] - 1) {
-                    cout << i << endl;
-                    cout << hash[i] << endl;
+            if (hash[s[i] - 'a'] == -1)
+                hash[s[i] - 'a'] = i;
+            else 
+                if (distance[s[i] - 'a'] != i - hash[s[i] - 'a'] - 1)
                     return false;
-                }
-            }
         }
         return true;
     }
 };
-int main()
-{
-    Solution().checkDistances("abaccb", { 1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 });
-    return 0;
-}
