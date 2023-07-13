322cd8f3935e9d2ef051f5fec7a08c70f6efd233
“changjingjing” <2249077862@qq.com>
2023-07-13 08:29:58 +0800

    leetcode 931. 下降路径最小和

--- C/leetcode/931. 下降路径最小和/main.cpp
+++ C/leetcode/931. 下降路径最小和/main.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+    int minFallingPathSum(vector<vector<int>>& arr) {
+        int n = arr.size();
+
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (j == 0) {
-                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1]);
+                    arr[i][j] += min(arr[i-1][0], arr[i-1][1]);
                 } else if (j == n - 1) {
-                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j-1]);
+                    arr[i][j] += min(arr[i-1][n-1], arr[i-1][n-2]);
                 } else {
-                    matrix[i][j] += min({matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1]});
+                    arr[i][j] += min({arr[i-1][j-1], arr[i-1][j], arr[i-1][j+1]});
                 }
             }
         }
+
         int result = INT_MAX;
         for (int i = 0; i < n; i++) {
-            if (result > matrix[n-1][i]) {
-                result = matrix[n-1][i];
+            if (arr[n-1][i] < result) {
+                result = arr[n-1][i];
             }
         }
+
         return result;
     }
 };
