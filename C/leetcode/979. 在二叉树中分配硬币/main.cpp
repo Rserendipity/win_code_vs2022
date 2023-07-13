9e5ceaebb8b001cf4d70c6b40a3df984969b7908
“changjingjing” <2249077862@qq.com>
2023-07-14 07:59:05 +0800

    leetcode 979. 在二叉树中分配硬币

---
+++ C/leetcode/979. 在二叉树中分配硬币/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int ret = 0;
+public:
+    int distributeCoins(TreeNode* root) {
+        dfs(root);
+        return ret;
+    }
+    int dfs(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        ret += abs(left) + abs(right);
+        return root->val - 1 + left + right;
+    }
+};
