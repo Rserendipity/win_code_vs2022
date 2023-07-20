bb07da0fa490a7b1532fb9dc3031e91f76da1e44
“changjingjing” <2249077862@qq.com>
2023-07-20 08:57:40 +0800

    leetcode 918. 环形子数组的最大和

---
+++ C/leetcode/918. 环形子数组的最大和/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> leftMax(n);
+        // 对坐标为 0 处的元素单独处理，避免考虑子数组为空的情况
+        leftMax[0] = nums[0];
+        int leftSum = nums[0];
+        int pre = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < n; i++) {
+            pre = max(pre + nums[i], nums[i]);
+            res = max(res, pre);
+            leftSum += nums[i];
+            leftMax[i] = max(leftMax[i - 1], leftSum);
+        }
+
+        // 从右到左枚举后缀，固定后缀，选择最大前缀
+        int rightSum = 0;
+        for (int i = n - 1; i > 0; i--) {
+            rightSum += nums[i];
+            res = max(res, rightSum + leftMax[i - 1]);
+        }
+        return res;
+    }
+};
