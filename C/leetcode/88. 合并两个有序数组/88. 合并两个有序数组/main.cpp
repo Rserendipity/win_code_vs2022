a3f5d8e3367fdaf6af65ef7f560954e8c5ace06e
“changjingjing” <2249077862@qq.com>
2023-08-13 19:28:59 +0800

    leetcode 88. 合并两个有序数组

--- C/leetcode/88. 合并两个有序数组/88. 合并两个有序数组/main.cpp
+++ C/leetcode/88. 合并两个有序数组/88. 合并两个有序数组/main.cpp
@@ -1,35 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
-	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-		int index = nums1.size() - 1;
-		int end1 = m - 1;
-		int end2 = n - 1;
-		while (end1 >= 0 && end2 >= 0) {
-			if (nums1[end1] > nums2[end2]) {
-				nums1[index] = nums1[end1];
-				index--;
-				end1--;
-			}
-			else {
-				nums1[index] = nums2[end2];
-				index--;
-				end2--;
-			}
-		}
-		while (end2 >= 0)
-		{
-			nums1[index] = nums2[end2];
-			index--;
-			end2--;
-		}
-	}
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int sz = nums1.size() - 1;
+        int end1 = m-1, end2 = n-1;
+        while (end1 >= 0 && end2 >= 0) {
+            if (nums1[end1] > nums2[end2]) {
+                nums1[sz--] = nums1[end1--];
+            } else {
+                nums1[sz--] = nums2[end2--];
+            }
+        }
+        while (end2 >= 0) {
+            nums1[sz--] = nums2[end2--];
+        }
+    }
 };
-int main()
-{
-
-    system("pause");
-    return 0;
-}
