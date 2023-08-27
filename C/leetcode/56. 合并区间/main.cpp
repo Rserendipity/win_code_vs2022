75d13de58bb7273ebb4ee18831487c6bca5d5783
“changjingjing” <2249077862@qq.com>
2023-08-27 16:08:43 +0800

    leetcode 56. 合并区间

---
+++ C/leetcode/56. 合并区间/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        vector<int> begin, end;
+        for (auto& v : intervals) {
+            begin.push_back(v[0]);
+            end.push_back(v[1]);
+        }
+        sort(begin.begin(), begin.end());
+        sort(end.begin(), end.end());
+        int n = intervals.size();
+        for(int i =0, j = 0; i< n; i++) {
+            if(i == n-1 || begin[i + 1] > end[i]) {
+                result.push_back({begin[j], end[i]});
+                j = i + 1;
+            }
+        }
+
+        return result;
+    }
+};
