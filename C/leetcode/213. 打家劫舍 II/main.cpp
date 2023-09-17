6fd07396a1c47b36390895bc1345d1d324c406af
“changjingjing” <2249077862@qq.com>
2023-09-17 13:20:18 +0800

    leetcode 213. 打家劫舍 II

--- C/leetcode/213. 打家劫舍 II/main.cpp
+++ C/leetcode/213. 打家劫舍 II/main.cpp
@@ -1,19 +1,23 @@
 class Solution {
     int _rob(vector<int>& nums, int begin, int end) {
-        if (begin >= end) 
-            return 0;
-        
-        int n = nums.size();
-        vector<int> dp(n);
-        dp[begin] = nums[begin];
-        for (int i = begin + 1; i < end; i++) {
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-        }
-        return dp[end - 1];
+      vector<int> dp(nums.size() + 2);
+      for (int i = begin + 2; i < end + 2; i++) {
+        dp[i] = max(dp[i - 2] + nums[i - 2], dp[i - 1]);
+      }
+        return dp[end + 1];
     }
+
 public:
     int rob(vector<int>& nums) {
+        // 偷 0 ~ n-2下标的
+        // 偷 1 ~ n-1下标的
+        // 取最大值
         int n = nums.size();
-        return max(nums[0] + _rob(nums, 2, n-1), _rob(nums, 1, n));
+        if (n == 1)
+            return nums[0];
+        // if (n == 2) return max(nums[0], nums[1]);
+
+        // n > 2
+        return max(_rob(nums, 0, n - 1), _rob(nums, 1, n));
     }
 };
